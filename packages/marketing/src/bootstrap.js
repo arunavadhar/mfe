a613472467002179c3ee7eca9b649bd49284d3a5
arunavadhar <arunavadhar13@gmail.com>
2024-11-28 14:45:40 -0600

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mount = (el) => {
+    ReactDOM.render(
+        <App/>, el
+    )
+}
+
+if(process.env.NODE_ENV == 'development') {
+    const el = document.querySelector('#marketing_dev_root');
+
+    if(el)
+        mount(el);
+}
+
+export { mount }
