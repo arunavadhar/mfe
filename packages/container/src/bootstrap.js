376488d00426f8ba6852c7da5ceac437ed902a94
arunavadhar <arunavadhar13@gmail.com>
2024-11-29 11:35:18 -0600

    trying workflow run8

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,6 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//adding comment to run workflow again
+//adding comment to run workflow now
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
